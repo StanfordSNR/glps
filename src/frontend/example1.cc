1db9641487c1fd13a2d99de5a2506e6b6c41dbf9
Keith Winstein <keithw@cs.stanford.edu>
2022-01-30 01:13:00 -0800

    example1: bug fix in synthesize_reflected_signal

--- src/frontend/example1.cc
+++ src/frontend/example1.cc
@@ -234,7 +234,7 @@ TimeDomainSignal synthesize_reflected_signal( const double tag_time_offset,
   auto delayed_tag_signal = delay( tag_signal, tag_time_offset, tag_fft, tag_ifft );
   auto delayed_transmitter_signal = delay( transmitter_signal, path_delay, transmitter_fft, transmitter_ifft );
   TimeDomainSignal reflected_signal { transmitter_signal.size(), transmitter_signal.sample_rate() };
-  for ( unsigned int i = 0; i < delayed_tag_signal.size(); i++ ) {
+  for ( unsigned int i = 0; i < reflected_signal.size(); i++ ) {
     reflected_signal.at( i )
       = path_gain * delayed_transmitter_signal.at( i ) * delayed_tag_signal.at( i % delayed_tag_signal.size() );
   }
